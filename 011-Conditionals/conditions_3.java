7e71378d4ec6aa5e9c28fb4ce0bc4f3d4e06907b
Dipesh <dipeshchauhan9898@gmail.com>
2025-12-28 21:22:10 +0530

    InitialFile

---
+++ 011-Conditionals/conditions_3.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class conditions_3{
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Signal: ");
+        String str = sc.nextLine();
+        
+        if(str.equals("green")){
+            System.out.println("You may move now");
+        }
+        else if(str.equals("red")){
+            System.out.println("STOP – Vehicles must halt");
+        }
+        else if(str.equals("yellow")){
+            System.out.println("WAIT – Get ready to stop");
+        }
+        else{
+            System.out.println("Invalid Input");
+        }
+    }
+}
