0e16a41bbb2fbb693e8131107b3b80300790eb3b
Dipesh <dipeshchauhan9898@gmail.com>
2025-12-31 20:27:35 +0530

    UpdateFile

--- 015-Array/multidimensional.java
+++ 015-Array/multidimensional.java
@@ -1,6 +1,7 @@
 class multidimensional{
     public static void main(String[] args){
-
+        
+        //2D array
         int[][] arr = new int[2][3];
 
         arr[0][0] = 101;
@@ -19,6 +20,27 @@ class multidimensional{
                 
             }
             System.out.println("");
+        }
+
+        //3D Array
+        int[][][] arr2 = {
+            {
+                {1,2,3},
+                {4,5,6}
+            },
+            {
+                {7,8,9},
+                {10,11,12}
+            }
+        };
+
+        System.out.println("Enlements of 3D array");
+        for(int i=0;i<arr2.length;i++){
+            for(int j=0;j<arr2[i].length;j++){
+                for(int k=0;k<arr2[i][j].length;k++){
+                    System.out.print(arr2[i][j][k]+" ");
+                }System.out.println("");
+            }System.out.println("-----");
         }
      }
 }
