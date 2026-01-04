f2f56aab9d356e7fa6b3f325ab3f58a684504e74
Dipesh <dipeshchauhan9898@gmail.com>
2026-01-04 22:35:58 +0530

    InitialFiles

---
+++ 018-VariableArgument/varArg.java
@@ -0,0 +1,22 @@
+class addition{
+
+    static double sum(double ...arr){
+
+        double result = 0;
+
+        for(double add : arr){
+            result+=add;
+        }
+
+        return result;
+    }
+}
+
+public class varArg{
+    public static void main(String[] args){
+        System.out.println("Addition of two no : "+addition.sum(5,5));
+        System.out.println("Addition of three no : "+addition.sum(5,5,5));
+        System.out.println("Addition of four no : "+addition.sum(5,5,5,5));
+        System.out.println("Addition of five no : "+addition.sum(5,5,5,5,5));
+    }
+}
