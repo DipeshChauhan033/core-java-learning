c0d1005762544065f7470a1faa35dc4325d1e8e9
Dipesh <dipeshchauhan9898@gmail.com>
2026-01-19 23:53:52 +0530

    UpdateFile

--- 026-Package/anagram_check/AnagramLogic.java
+++ 026-Package/anagram_check/AnagramLogic.java
@@ -8,23 +8,58 @@ class anagramLogic{
     public String str2;
     public int s1length = 0;
     public int s2length = 0;
+    public int count1 = 0;
+    public int count2 = 0;
+
 
     public anagramLogic(String str1,String str2){
         this.str1 = str1;
         this.str2 = str2;
     }
     public void check_anagram(){
+
+        str1 = str1.replaceAll("\\s", "").toLowerCase();
+        str2 = str2.replaceAll("\\s", "").toLowerCase();
+
         CheckLength obj = new CheckLength(str1);
         obj.length_num();
         s1length = obj.count;
 
         CheckLength obj2 = new CheckLength(str2);
         obj2.length_num();
-        s2length = obj.count;
+        s2length = obj2.count;
+
+        char[] carr1 = str1.toCharArray();
+        char[] carr2 = str2.toCharArray();
+
+
+        for(int i=0;i<carr1.length;i++){
+
+            if(s1length != s2length){
+                System.out.println("String are not anagram");
+                break;
+            }
+
+            for(int j=0;j<carr1.length;j++){
+                if(carr1[i] == carr1[j]){
+                    count1++;
+                }
+
+                if(carr1[i] == carr2[j]){
+                    count2++;
+                }
+            }
+        }
 
         if(s1length == s2length){
-            
+            if(count1!=count2){
+                    System.out.println("String are not anagram");
+            }
+            else{
+                System.out.println("String is anagram");
+            }
         }
+        
     }
     
 }
