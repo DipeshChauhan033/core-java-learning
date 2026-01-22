614661dbc475378894bc3735731adf877fde73e1
Dipesh <dipeshchauhan9898@gmail.com>
2026-01-23 00:46:00 +0530

    UpdateFile

--- 025-Interface/Login.java
+++ 025-Interface/Login.java
@@ -1,11 +1,57 @@
+import java.util.Scanner;
+
 interface Uname{
-    void username(uname);
+    void username(String uname);
 }
 
 interface Pass{
-    void password(pass);
+    void password(int pass);
 }
 
 class Another{
-    System.out.println("Enter Facebook to login with Facebook\nEnter Gmail to login with Gmail:");
+    String s;
+
+    public void throughOther(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Facebook to login with Facebook\nEnter Gmail to login with Gmail\nEnter None to login with uname&pass :");
+        s = sc.nextLine().toUpperCase();
+
+        if(s.equals("FACEBOOK")){
+            System.out.println("Login done with facebook");
+        }
+        else if(s.equals("GMAIL")){
+            System.out.println("Login done with Gmail");
+        }
+        else{
+            Uname obj = new Display();
+            obj.username("Hopper");
+
+            Pass obj1 = new Display();
+            obj1.password(123456);
+        } 
+    }
+}
+
+class Display extends Another implements Uname,Pass{
+    
+    public void username(String uname){
+        System.out.println("Username: "+uname);
+    }
+
+    public void password(int pass){
+        System.out.println("Password: "+pass);
+    }
+
+    void System(){
+        super.throughOther();
+    }
+
+}
+
+public class Login{
+    public static void main(String[] args){
+        Display obj = new Display();
+        obj.System();
+        
+    }
 }
