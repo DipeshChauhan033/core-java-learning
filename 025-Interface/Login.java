9a5d70813e867ca10f88cf2f22a4ac5a39493308
Dipesh <dipeshchauhan9898@gmail.com>
2026-01-22 23:59:17 +0530

    InitialFile

---
+++ 025-Interface/Login.java
@@ -0,0 +1,11 @@
+interface Uname{
+    void username(uname);
+}
+
+interface Pass{
+    void password(pass);
+}
+
+class Another{
+    System.out.println("Enter Facebook to login with Facebook\nEnter Gmail to login with Gmail:");
+}
