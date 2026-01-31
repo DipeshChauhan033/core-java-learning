df89b907898aa902e40b1b02e40f1a78241dc185
Dipesh <dipeshchauhan9898@gmail.com>
2026-01-31 23:06:16 +0530

    InitialFile

---
+++ 029-Multithreading/ThreadPriority/tempCodeRunnerFile.java
@@ -0,0 +1,28 @@
+public class SetPriority extends Thread{
+    public SetPriority(String msg){
+        super(msg);
+    }
+
+    @Override
+    public void run(){
+        for(int i=0;i<=10;i++){
+            try{
+                Thread.sleep(100);
+            }
+            catch(Exception e){}
+                System.out.println("ThreadName: "+Thread.currentThread().getName()+", "+"Priority: "+Thread.currentThread().getPriority()+", Count "+ i);
+        }
+    }
+
+    public static void main(String[] args){
+        SetPriority l = new SetPriority("Low Priority");
+        SetPriority m = new SetPriority("Medium Priority");
+        SetPriority h = new SetPriority("High Priority");
+        l.setPriority(Thread.MIN_PRIORITY);
+        m.setPriority(Thread.MIN_PRIORITY);
+        h.setPriority(Thread.MIN_PRIORITY);
+        l.start();
+        m.start();
+        h.start();
+    }
+}
