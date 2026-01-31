35f4beaacaf3da2de34178f4cdb0b1fda9c5059f
Dipesh <dipeshchauhan9898@gmail.com>
2026-01-31 23:58:22 +0530

    InitialFiles

--- 029-Multithreading/Yield/YieldMethod.java
+++ 029-Multithreading/Yield/YieldMethod.java
@@ -1,9 +1,20 @@
 class Tmp extends Thread{
     @Override
     public void run(){
+        
         for(int i = 0;i<=5;i++){
             System.out.println("Thread: "+Thread.currentThread().getName() + " Is running");
-            Thread.yield();  //Give change to another process
+        }
+    } 
+}
+
+class Tmp1 extends Thread{
+    @Override
+    public void run(){
+        
+        for(int i = 0;i<=5;i++){
+            System.out.println("Thread: "+Thread.currentThread().getName() + " Is running");
+            Thread.yield();
         }
     } 
 }
@@ -12,8 +23,11 @@ public class YieldMethod{
     public static void main(String[] args){
         Tmp obj1 = new Tmp();
         Tmp obj2 = new Tmp();
-
+        System.out.println("Before Using yield method...");
         obj1.start();
         obj2.start();
+
+        
+
     }
 }
